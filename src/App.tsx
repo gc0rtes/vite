492725e600bb8213b33ee735044364e2ba1b29d5
Guilherme <guilhermeco@gmail.com>
2024-10-30 09:34:21 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,97 @@
+import { ChannelFilters, ChannelOptions, ChannelSort } from 'stream-chat';
+import {
+  Channel,
+  ChannelHeader,
+  ChannelList,
+  Chat,
+  MessageInput,
+  VirtualizedMessageList as MessageList,
+  Thread,
+  Window,
+  useCreateChatClient,
+  ThreadList,
+  ChatView,
+} from 'stream-chat-react';
+import 'stream-chat-react/css/v2/index.css';
+
+const params = (new Proxy(new URLSearchParams(window.location.search), {
+  get: (searchParams, property) => searchParams.get(property as string),
+}) as unknown) as Record<string, string | null>;
+
+const parseUserIdFromToken = (token: string) => {
+  const [, payload] = token.split('.');
+
+  if (!payload) throw new Error('Token is missing');
+
+  return JSON.parse(atob(payload))?.user_id;
+};
+
+const apiKey = params.key ?? (import.meta.env.VITE_STREAM_KEY as string);
+const userToken = params.ut ?? (import.meta.env.VITE_USER_TOKEN as string);
+const userId = parseUserIdFromToken(userToken);
+
+const filters: ChannelFilters = {
+  members: { $in: [userId] },
+  type: 'messaging',
+};
+const options: ChannelOptions = { limit: 3, presence: true, state: true };
+const sort: ChannelSort = { last_message_at: -1, updated_at: -1 };
+
+type LocalAttachmentType = Record<string, unknown>;
+type LocalChannelType = Record<string, unknown>;
+type LocalCommandType = string;
+type LocalEventType = Record<string, unknown>;
+type LocalMessageType = Record<string, unknown>;
+type LocalPollOptionType = Record<string, unknown>;
+type LocalPollType = Record<string, unknown>;
+type LocalReactionType = Record<string, unknown>;
+type LocalUserType = Record<string, unknown>;
+
+type StreamChatGenerics = {
+  attachmentType: LocalAttachmentType;
+  channelType: LocalChannelType;
+  commandType: LocalCommandType;
+  eventType: LocalEventType;
+  messageType: LocalMessageType;
+  pollOptionType: LocalPollOptionType;
+  pollType: LocalPollType;
+  reactionType: LocalReactionType;
+  userType: LocalUserType;
+};
+
+const App = () => {
+  const chatClient = useCreateChatClient<StreamChatGenerics>({
+    apiKey,
+    tokenOrProvider: userToken,
+    userData: { id: userId },
+  });
+
+  if (!chatClient) return <>Loading...</>;
+
+  return (
+    <Chat client={chatClient}>
+      <ChatView>
+        <ChatView.Selector />
+        <ChatView.Channels>
+          <ChannelList filters={filters} options={options} sort={sort} />
+          <Channel>
+            <Window>
+              <ChannelHeader />
+              <MessageList returnAllReadData />
+              <MessageInput focus />
+            </Window>
+            <Thread virtualized />
+          </Channel>
+        </ChatView.Channels>
+        <ChatView.Threads>
+          <ThreadList />
+          <ChatView.ThreadAdapter>
+            <Thread virtualized />
+          </ChatView.ThreadAdapter>
+        </ChatView.Threads>
+      </ChatView>
+    </Chat>
+  );
+};
+
+export default App;
